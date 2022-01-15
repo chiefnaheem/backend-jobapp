137a2f4d85072e4bc72bf59a5a216b95d5b3fcae
naheem adedokun <naheemadedokun21@gmail.com>
2022-01-15 22:40:31 +0100

    fixing bugs

---
+++ src/database/mongoConnect.ts
@@ -0,0 +1,51 @@
+/* eslint-disable no-console */
+// import mongoose from 'mongoose';
+// import winston, { format } from 'winston';
+
+// const logger = winston.createLogger({
+//   format: format.combine(format.simple()),
+//   transports: [
+//     new winston.transports.Console(),
+//   ],
+// });
+
+// const connectDB = () => {
+//   const url: string = process.env.NODE_ENV === 'production' ? process.env.DATABASE_URL as string : 'mongodb://127.0.0.1:27017/job-finder-api';
+//   mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+//     .then(() => {
+//       logger.log('info', 'Successfully connected to MongoDB Atlas!');
+//     }).catch((error) => {
+//       logger.log('error', error.message);
+//     });
+// };
+// export default connectDB;
+
+// --unhandled-rejections=strict
+import mongoose from "mongoose";
+
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+require('dotenv').config();
+
+export const connectDB = () => {
+  try {
+    mongoose.connect('mongodb+srv://chiefnaheem:£NAHeem199@cluster0.styhr.mongodb.net/JOB-APP?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then(() => {
+      // console.log("Connected to DB");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const connectTestDB = () => {
+  try{
+    MongoMemoryServer.create().then((mongo) => {
+      const uri:any = mongo.getUri();
+      mongoose.connect(uri).then(() => {
+        // console.log("connected to testDB");
+      });
+    });
+  }
+  catch (error:any) {
+    console.log(error);
+  }
+};
